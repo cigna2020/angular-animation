033968ef226d82578a0b62b475d1bb92811a8391
Alex <cigan@ua.fm>
2021-05-04 12:27:42 +0300

    change the state of animation by clicking a button

--- ng-animation/src/app/app.component.ts
+++ ng-animation/src/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import {state, style, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('boxAnim', [
+      state('start', style({background: 'blue'})),
+      state('end', style({
+        backgroundColor: 'red',
+        transform: 'scale(1.2)',
+        transition: 'all, 1s'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
+  boxState = 'start'
+
+  animate() {
+    this.boxState = this.boxState === 'end' ? 'start' : 'end';
+  }
 }
