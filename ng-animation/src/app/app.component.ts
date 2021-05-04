78681fb6dee6c0efd59c68554e09413a279f5b6f
Alex <cigan@ua.fm>
2021-05-04 12:46:24 +0300

    difficult transition with steps

--- ng-animation/src/app/app.component.ts
+++ ng-animation/src/app/app.component.ts
@@ -12,8 +12,20 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
         backgroundColor: 'red',
         transform: 'scale(1.2)',
       })),
+      state('special', style({
+        background: 'orange',
+        transform: 'scale(0.5)',
+        borderRadius: '50%'
+      })),
       transition('start => end', animate(450)),
-      transition('end => start', animate('800ms ease-in-out'))
+      transition('end => start', animate('800ms ease-in-out')),
+      // transition('special <=> *', animate(500)) // * - какое-либо состояние
+      transition('special <=> *', [
+        style({background: 'green'}),
+        animate('1s', style({
+          background: 'pink'
+        })), animate(750)
+      ])
     ])
   ]
 })
